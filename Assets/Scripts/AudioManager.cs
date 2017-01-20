bace8928fb6fc90405fe534dd8582b2457972458
Joel Lopez <jowie.jowie@gmail.com>
2017-01-20 23:35:15 +0100

    Add basic audio manager and gameplay manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] GameplayManager gameplay;
+
+    private AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.loop = false;
+    }
+
+    void Update()
+    {
+        if (!source.isPlaying)
+            gameplay.EndGame();
+    }
+
+    public void PlaySound(string sound, uint delay)
+    {
+        AudioClip clip = (AudioClip) Resources.Load(sound);
+        source.clip = clip;
+        source.Play(delay);
+    }
+}
---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    public enum ScoreType
+    {
+        Miss,
+        Bad,
+        Ok,
+        Good,
+        Perfect
+    }
+
+    public AudioManager gameAudio;
+    public const uint audioDelay = 1;
+    public bool smashMode = false;
+
+    private int[] scores = {0, 0, 0, 0};
+    private int[] gauge = {0, 0, 0, 0};
+
+    void Start ()
+    {
+        gameAudio.PlaySound("", audioDelay);
+        smashMode = false;
+    }
+    
+    void Update ()
+    {
+        
+    }
+
+    public void Score(int player, ScoreType type)
+    {
+        scores[player] = 50 * (int)type;
+        gauge[player] = 2 * (int) type - 1;
+    }
+
+    public void EndGame()
+    {
+        // TODO: Call GameManager here to end game
+    }
+}
