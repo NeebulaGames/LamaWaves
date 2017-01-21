2b93f3cee005ff66833a4a6195ac57cd763f23f7
Pol Vilà Saló <pol.vila@tangelogames.com>
2017-01-21 16:11:55 +0100

    Update prefabs and scene

--- Assets/SortingRenderer.cs
+++ Assets/SortingRenderer.cs
@@ -6,16 +6,22 @@ public class SortingRenderer : MonoBehaviour {
 
 	//public int sortingOrder;
 	//public string sortingLayerName;
+	public Material mat;
 
 	// Use this for initialization
 	void Start () {
 		//transform.GetComponent<Renderer>().sortingLayerName = sortingLayerName;
 		//transform.GetComponent<Renderer>().sortingOrder = sortingOrder;
-		transform.GetComponent<Material>().renderQueue = 6000;
+		mat = transform.GetComponent<ParticleSystemRenderer>().material;
+		mat.renderQueue = 3001;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(gameObject.name + " " + transform.GetComponent<Renderer>().sortingLayerName + " " + transform.GetComponent<Renderer>().sortingOrder);
+		mat.renderQueue = 3001;
+		//transform.GetComponent<ParticleSystemRenderer>().material.renderQueue = 3001;
+		//Debug.Log(gameObject.name + " " + transform.GetComponent<Renderer>().sortingLayerName + " " + transform.GetComponent<Renderer>().sortingOrder);
 	}
+
 }
+
