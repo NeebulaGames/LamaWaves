8360f3964a73c1791f72548dff0c09897727d4eb
Joel Lopez <jowie.jowie@gmail.com>
2017-01-22 13:30:43 +0100

    Add credits menu and missing sprites in play menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class MainMenuManager : MonoBehaviour
 {
     public GameObject mainMenu;
+    public GameObject creditsMenu;
     
     void OnEnable()
     {
         mainMenu.SetActive(true);
+        creditsMenu.SetActive(false);
     }
 
     void OnDisable()
     {
         mainMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+    }
+
+    public void OpenCredits()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        mainMenu.SetActive(true);
+        creditsMenu.SetActive(false);
     }
 }
